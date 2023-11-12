c014600dfb5831ec048728c493bab38732a2bf08
areian13 <iantyrho13@naver.com>
2023-11-12 20:54:49 +0900

    Project Start

---
+++ src/Server/Data.java
@@ -0,0 +1,7 @@
+package Server;
+
+import java.util.Scanner;
+
+public interface Data {
+    public void scan(Scanner scanner);
+}
---
+++ src/Server/Exercise.java
@@ -0,0 +1,6 @@
+package Server;
+
+public class Exercise {
+    String name;
+    int cal;
+}
